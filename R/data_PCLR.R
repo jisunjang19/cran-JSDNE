1e8ccd954027018f074e8bf9f598da4781ce7690
Jisun Jang <146486396+jisunjang19@users.noreply.github.com>
2024-02-21 10:28:21 +0000

    Delete R directory

--- R/data_PCLR.R
+++
@@ -1,9 +0,0 @@
-
-#' Database to create DNE_PCLR method
-#'
-#' data_PCLR with 6 variables: Cluster, Total DNE/Total Polygon Faces, Mean value of DNE on Apex, IQR of DNE on Whole surface, Median value of DNE on Whole surface, Mean value of DNE on Convex surface
-#'
-#' The database was obtained by calculating the DNE on the auricular surface from European collections.
-#' The specimens are belongs to  the Louis Lopes collection in Lisbon, the 21st Century Identified Human Remains Collection in Coimbra, and the CAL Milano Cemetery Skeletal Collection in Milan.
-#'
-"data_PCLR"
--- R/data_PCR.R
+++
@@ -1,9 +0,0 @@
-
-#' Database to create DNE_PCR method
-#'
-#' data_PCR with 4 variables: Age, Median value of DNE on Apex, Area having Macroporosity, IQR of DNE on Whole surface
-#'
-#' The database was obtained by calculating the DNE on the auricular surface from European collections.
-#' The specimens are belongs to  the Louis Lopes collection in Lisbon, the 21st Century Identified Human Remains Collection in Coimbra, and the CAL Milano Cemetery Skeletal Collection in Milan.
-#'
-"data_PCR"
