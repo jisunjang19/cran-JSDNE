7cc8547d119d2eea3817320c42d3074a9e6d7eb2
Jisun Jang <146486396+jisunjang19@users.noreply.github.com>
2024-03-03 21:23:11 +0000

    Delete R directory

--- R/data_PCR.R
+++
@@ -1,9 +0,0 @@
-
-#' Database to create DNE_PCR method
-#'
-#' data_PCR with 8 variables: Age, Median value of DNE on Apex, IQR of DNE on Apex, Mean value of DNE on Convex surface, Mean value of DNE on Concave surface, Proportion having DNE under 0.0001, Proportion having DNE over 0.6, Median value of DNE on Whole surface
-#'
-#' The database was obtained by calculating the DNE on the auricular surface from European collections.
-#' The specimens are belongs to  the Louis Lopes collection in Lisbon, the 21st Century Identified Human Remains Collection in Coimbra, and the CAL Milano Cemetery Skeletal Collection in Milan.
-#'
-"data_PCR"
