e5923c951de489463bdd4a6c70bbbd1812ceeb92
Jisun Jang <146486396+jisunjang19@users.noreply.github.com>
2024-03-03 21:24:31 +0000

    Add files via upload

---
+++ R/data_PCLR.R
@@ -0,0 +1,9 @@
+
+#' Database to create DNE_PCLR method
+#'
+#' data_PCLR with 6 variables: Cluster, Total DNE/Total Polygon Faces, Mean value of DNE on Apex, IQR of DNE on Whole surface, Median value of DNE on Whole surface, Mean value of DNE on Convex surface
+#'
+#' The database was obtained by calculating the DNE on the auricular surface from European collections.
+#' The specimens are belongs to  the Louis Lopes Collection in Lisbon, the 21st Century Identified Human Remains Collection in Coimbra, and the CAL Milano Cemetery Skeletal Collection in Milan.
+#'
+"data_PCLR"
