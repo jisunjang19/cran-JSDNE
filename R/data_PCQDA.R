25d729550b5d0965f93244e8b4e304855c109d35
Jisun Jang <146486396+jisunjang19@users.noreply.github.com>
2024-04-16 13:08:57 +0100

    Delete R directory

--- R/data_PCQDA.R
+++
@@ -1,9 +0,0 @@
-
-#' Databases to create DNE_PCQDA method.
-#'
-#' data_PCQDA with 5 variables: Cluster, Proportion having DNE under 0.0001, Proportion having DNE over 0.6, Mean value of DNE on Apex, Total DNE/Total Polygon Faces
-#'
-#' The databases was obtained by calculating the DNE on the auricular surface from European collections.
-#' The specimens are belongs to  the Louis Lopes Collection in Lisbon, the 21st Century Identified Human Remains Collection in Coimbra, and the CAL Milano Cemetery Skeletal Collection in Milan.
-#'
-"data_PCQDA"
